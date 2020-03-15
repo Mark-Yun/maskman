99d49c9e8da6b24a479342bd9e98849c3a18b9fd
MarkYun <mark.yun89@gmail.com>
2020-03-15 20:04:35 +0900

    Fix DateUtil bug

--- app/src/main/java/com/mark/zumo/client/customer/util/DateUtils.java
+++ app/src/main/java/com/mark/zumo/client/customer/util/DateUtils.java
@@ -36,20 +36,47 @@ public class DateUtils {
             return "";
         }
 
-        Calendar calendar = Calendar.getInstance(); // creates a new calendar instance
-        calendar.setTime(createdDate);   // assigns calendar to given date
+        Calendar targetCalendar = Calendar.getInstance(); // creates a new calendar instance
+        targetCalendar.setTime(createdDate);   // assigns calendar to given date
 
-        Calendar currentTime = Calendar.getInstance(Locale.getDefault());
+        int targetDay = targetCalendar.get(Calendar.DAY_OF_YEAR);
+        int targetHour = targetCalendar.get(Calendar.HOUR_OF_DAY);// gets hour in 24h format
+        int targetMinute = targetCalendar.get(Calendar.MINUTE);// gets month number, NOTE this is zero based!
+        int targetSeconds = targetCalendar.get(Calendar.SECOND);// gets month number, NOTE this is zero based!
+        Log.d(TAG, "convertTimeStamp: targetDay=" + targetDay
+                + " targetHour=" + targetHour
+                + " targetMinute=" + targetMinute
+                + " targetSeconds=" + targetSeconds);
+
+        Calendar currentCalendar = Calendar.getInstance(Locale.getDefault());
+
+        int currentDay = currentCalendar.get(Calendar.DAY_OF_YEAR);
+        int currentHour = currentCalendar.get(Calendar.HOUR_OF_DAY);// gets hour in 24h format
+        int currentMinute = currentCalendar.get(Calendar.MINUTE);// gets month number, NOTE this is zero based!
+        int currentSeconds = currentCalendar.get(Calendar.SECOND);// gets month number, NOTE this is zero based!
+        Log.d(TAG, "convertTimeStamp: currentDay=" + currentDay
+                + " currentHour=" + currentHour
+                + " currentMinute=" + currentMinute
+                + " currentSeconds=" + currentSeconds);
+
         Calendar gapCalendar = Calendar.getInstance();
-        gapCalendar.setTimeInMillis(currentTime.getTimeInMillis() - calendar.getTimeInMillis());
+        gapCalendar.set(Calendar.DAY_OF_YEAR, currentDay - targetDay + 1);
+        gapCalendar.set(Calendar.HOUR_OF_DAY, currentHour - targetHour);
+        gapCalendar.set(Calendar.MINUTE, currentMinute - targetMinute);
+        gapCalendar.set(Calendar.SECOND, currentSeconds - targetSeconds);
 
         int gapDay = gapCalendar.get(Calendar.DAY_OF_YEAR);
         int gapHour = gapCalendar.get(Calendar.HOUR_OF_DAY);// gets hour in 24h format
         int gapMinute = gapCalendar.get(Calendar.MINUTE);// gets month number, NOTE this is zero based!
         int gapSeconds = gapCalendar.get(Calendar.SECOND);// gets month number, NOTE this is zero based!
 
+        Log.d(TAG, "convertTimeStamp: gapDay=" + gapDay
+                + " gapHour=" + gapHour
+                + " gapMinute=" + gapMinute
+                + " gapSeconds=" + gapSeconds);
+
         if (gapDay > 1) {
-            return String.format("%d일 전", gapDay-1);
+            return String.format("%d일 전", gapDay - 1);
         }
 
         if (gapHour > 0) {
